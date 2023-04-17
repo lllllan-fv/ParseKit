7e3993c0fe61f8566e051b614e6a565e7ffb006b
lllllan-fv <342310798@qq.com>
2023-04-18 00:25:33 +0800

    增加必要注释

--- parse.go
+++ parse.go
@@ -7,9 +7,18 @@ import (
 	"github.com/spf13/cast"
 )
 
+// 字符串解析
+// 字符串内容包含：数字和字符 ',[]{}'
+// [] 内表示固定顺序的内容，{} 内表示无序的内容，解析字符串获得所有可能的数字排序
+// eg: [1,2,3] -> [1,2,3]
+// eg: {1,2,3} -> [1,2,3] [1,3,2] [2,1,3] [2,3,1] [3,1,2] [3,2,1]
+// eg: {1,[2,3]} -> [1,2,3] [2,3,1]
+// eg: [1,{2,3}] -> [1,2,3] [1,3,2]
 func Parse(rule string) (ans [][]int, err error) {
 	if rules, err := parseSquareBracket(rule); err == nil {
+		// 如果当前字符串格式为 [...]，则对其中内容进行组合拼接
 		var mulAns [][][]int
+		// 对其所有子部分进行递归解析
 		for _, rule := range rules {
 			subAns, err := Parse(rule)
 			if err != nil {
@@ -20,6 +29,7 @@ func Parse(rule string) (ans [][]int, err error) {
 
 		return combine(mulAns), nil
 	} else if rules, err := parseBracket(rule); err == nil {
+		// 如果当前字符串格式为 {...}，则对其中内容进行全排列
 		var mulAns [][][]int
 		for _, rule := range rules {
 			subAns, err := Parse(rule)
@@ -32,6 +42,7 @@ func Parse(rule string) (ans [][]int, err error) {
 		return permute(mulAns), nil
 	}
 
+	// 不符合上述格式，则作为单个数字看待
 	i, err := cast.ToIntE(rule)
 	if err != nil {
 		return nil, err
@@ -40,12 +51,14 @@ func Parse(rule string) (ans [][]int, err error) {
 	return [][]int{{i}}, nil
 }
 
+// 三维数组全排列组成二维数组，示例见 TestPermute
 func permute(arrs [][][]int) (res [][]int) {
 	var vis = make([]int, len(arrs))
 	permuteDFS(arrs, 0, vis, []int{}, &res)
 	return res
 }
 
+// 回溯实现全排列
 func permuteDFS(arrs [][][]int, dep int, vis, cur []int, res *[][]int) {
 	if dep == len(arrs) {
 		var tmp = make([]int, len(cur))
@@ -58,21 +71,22 @@ func permuteDFS(arrs [][][]int, dep int, vis, cur []int, res *[][]int) {
 		if vis[index] == 1 {
 			continue
 		}
-		vis[index] = 1
 
+		vis[index] = 1
 		for _, sub := range arr {
 			permuteDFS(arrs, dep+1, vis, append(cur, sub...), res)
 		}
-
 		vis[index] = 0
 	}
 }
 
+// 三维数组组合拼接成二维数组，示例见 TestCombine
 func combine(arrs [][][]int) (res [][]int) {
 	combineDFS(arrs, 0, []int{}, &res)
 	return
 }
 
+// 递归实现拼接
 func combineDFS(arrs [][][]int, pos int, cur []int, res *[][]int) {
 	if pos == len(arrs) {
 		var tmp = make([]int, len(cur))
@@ -86,6 +100,7 @@ func combineDFS(arrs [][][]int, pos int, cur []int, res *[][]int) {
 	}
 }
 
+// 字符串满足 {...} 格式时按照最大颗粒拆分
 func parseBracket(rule string) (rules []string, err error) {
 	if !isBracketed(rule) {
 		return nil, errors.New("rule not bracketed")
@@ -93,6 +108,7 @@ func parseBracket(rule string) (rules []string, err error) {
 	return split(rule[1 : len(rule)-1]), nil
 }
 
+// 字符串满足 [...] 格式时按照最大颗粒拆分
 func parseSquareBracket(rule string) (rules []string, err error) {
 	if !isSquareBracketed(rule) {
 		return nil, errors.New("rule not square bracketed")
@@ -100,18 +116,13 @@ func parseSquareBracket(rule string) (rules []string, err error) {
 	return split(rule[1 : len(rule)-1]), nil
 }
 
+// 字符串拆分，',' 为分隔符， '[] {}' 看作完整单位
 func split(s string) []string {
+	var left = 0
+	var start = 0
 	var result []string
 
-	// 记录左括号出现的数量
-	left := 0
-
-	// 记录当前部分的起始位置
-	start := 0
-
-	for i := 0; i < len(s); i++ {
-		c := s[i]
-
+	for i, c := range s {
 		switch c {
 		case ',':
 			// 如果当前是逗号，而且左括号数量为 0，说明这个部分已经结束
@@ -136,10 +147,12 @@ func split(s string) []string {
 	return result
 }
 
+// 是否 {} 包裹
 func isBracketed(str string) bool {
 	return strings.HasPrefix(str, "{") && strings.HasSuffix(str, "}")
 }
 
+// 是否 [] 包裹
 func isSquareBracketed(str string) bool {
 	return strings.HasPrefix(str, "[") && strings.HasSuffix(str, "]")
 }
